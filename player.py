d858e7c6050973cf5869870dcc8930913356d746
John Russel Garcia <30710854+johnrusselmoleragarcia@users.noreply.github.com>
2019-11-29 19:31:37 +0800

    Card passing partially working
    Bugs: not waiting for other players to connect or pass card first before updating state

--- player.py
+++ player.py
@@ -1,16 +1,23 @@
 class Player:
-    def __init__(self):
+    def __init__(self, player_id):
+        self.player_id = player_id
         self.cards = []
+
+    def get_player_id(self):
+        return self.player_id
 
     def add_card(self, card):
         self.cards.append(str(card))
 
+    def remove_card(self, card_no):
+        return self.cards.pop(card_no)
+
     def show_cards(self):
-        print("Your cards:", end=" ")
+        print("Player", self.player_id, "cards:", end=" ")
         for card in self.cards:
             print(card, end=" ")
         print()
 
-    def pass_card(self):
-        n = int(input("Select a card to pass [1-4]: "))
-        self.cards.pop(n-1)
+    def pass_card(self, card_no):
+        self.cards.pop(card_no)
+
--- server.py
+++ server.py
@@ -37,7 +37,7 @@ class Deck:
             return self.cards.pop(0)
 
 
-server = "192.168.0.48"
+server = "192.168.100.11"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -52,30 +52,40 @@ print("Server Started, Waiting for connection")
 
 deck = Deck()
 deck.shuffle()
-players = [Player() for _ in range(max_players)]
+players = [Player(i+1) for i in range(max_players)]
 for n in range(max_players):
     for _ in range(4):
         players[n].add_card(deck.deal())
 
 
-def threaded_client(conn, player_no):
-    conn.send(pickle.dumps(players[player_no]))
-    reply = ""
+def show_all_cards():
+    for i in range(max_players):
+        players[i].show_cards()
+
+
+def threaded_client(conn, p_no):
+    conn.send(pickle.dumps(players[p_no]))
     while True:
         try:
             data = pickle.loads(conn.recv(2048))
-            players[player_no] = data
 
             if not data:
-                print("Player", player_no + 1, "disconnected")
+                print("Player", p_no + 1, "disconnected")
                 break
             else:
-                reply = players[player_no]
+                card_no = int(data)
+                passed_card = players[p_no].remove_card(card_no)
 
-                print("Received: ", data)
-                print("Sending :", reply)
+                print("Player", p_no + 1, "passed", passed_card)
+                if p_no < max_players - 1:
+                    print("Player", p_no + 2, "received", passed_card)
+                    players[p_no+1].add_card(passed_card)
+                else:
+                    print("Player 1 received", passed_card)
+                    players[0].add_card(passed_card)
 
-            conn.sendall(pickle.dumps(reply))
+            show_all_cards()
+            conn.sendall(pickle.dumps(players[p_no]))
         except:
             break
 
@@ -85,14 +95,13 @@ def threaded_client(conn, player_no):
 current_player = 0
 while True:
     conn, addr = s.accept()
-    print("Player", current_player+1, "connected to:", addr)
+    print("Player", current_player + 1, "connected to:", addr)
 
     start_new_thread(threaded_client, (conn, current_player))
     current_player += 1
 
     if current_player == max_players:
         print("All players are connected, Initializing Game")
-        for i in range(max_players):
-            players[i].show_cards()
+        show_all_cards()
     else:
         print("Waiting for other players to connect")
